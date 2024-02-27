9a37d65a4a2994d993c583f28865a8070240492a
Halyna-Mukha <msaarf555@gmail.com>
2024-02-27 13:20:52 +0100

    update

--- js/task-1.js
+++ js/task-1.js
@@ -2,14 +2,34 @@ const categoriesList = document.querySelectorAll('#categories .item');
 console.log(`Number of categories: ${categoriesList.length}`);
 
 categoriesList.forEach(element => {
-const titleEl = element.querySelector('h2').textContent;
-console.log(`Category: ${titleEl}`);
-const liCountAll = element.querySelectorAll('ul li').length;  
-console.log(`Elements: ${liCountAll}`);
+    const titleEl = element.querySelector('h2').textContent;
+    console.log(`Category: ${titleEl}`);
+    const liCountAll = element.querySelectorAll('ul li').length;
+    console.log(`Elements: ${liCountAll}`);
 });
 
-categoriesList.classList.add('menu-list');
 
-const mainItemEl = categoriesList.querySelector('.childElement');
+const parentUl = document.getElementById('categories');
 
-mainItemEl.classList.add('item')
+parentUl.classList.add('categories');
+
+const items = parentUl.querySelectorAll('.item');
+
+items.forEach(item => {
+    item.classList.add('item-list');
+
+    const nextUl = item.querySelector('ul');
+
+    if (nextUl) {
+        nextUl.classList.add('menu-item');
+
+        const nextItemsEl = nextUl.querySelectorAll('li');
+
+        nextItemsEl.forEach(nextItemEl => {
+            nextItemEl.classList.add('menu-title-list');
+        });
+    }
+});
+
+const titleEl = document.querySelectorAll('h2');
+titleEl.forEach(element => element.classList.add('menu-title'));
