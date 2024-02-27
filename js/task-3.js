1c2d007befa2a8b87448af205d04bede2ea6a90f
Halyna-Mukha <msaarf555@gmail.com>
2024-02-27 10:37:48 +0100

    update

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,13 @@
+const inputEl = document.querySelector('#name-input');
+inputEl.classList.add('input-field');
+
+const titleEl = document.querySelector('h1');
+titleEl.classList.add('title-text');
+
 
 const span = document.querySelector('#name-output');
 const input = document.addEventListener("input", handlerInput)
 function handlerInput(event) {
   console.log(event.target.value);
   span.textContent = event.target.value.trim() || 'Anonymous'
-}
+};
